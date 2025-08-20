eb30703a6060fa163721beb4252a809d47851806
Nikhil Kumar <nikhilkumar@Nikhils-MacBook-Air.local>
2025-08-20 15:52:08 +0530

    whatsapp message add

--- src/components/global/whatsapp/Whatsapp.jsx
+++ src/components/global/whatsapp/Whatsapp.jsx
@@ -2,11 +2,19 @@ import Link from 'next/link'
 import React from 'react'
 
 function Whatsapp() {
+  // Encode the default message for the WhatsApp URL
+  const defaultMessage = encodeURIComponent(
+    "Hi team PRAKRIA, I wish to enquire about my Website Development requirement."
+  );
+  const whatsappUrl = `https://wa.me/+919810810034?text=${defaultMessage}`;
+
   return (
     <div id="whatsappp">
-      <Link target='_blank' href="https://wa.me/+919810810034"><img src="/images/whatsapp.png" alt="" /></Link>
+      <Link target='_blank' href={whatsappUrl}>
+        <img src="/images/whatsapp.png" alt="WhatsApp" />
+      </Link>
     </div>
-  )
+  );
 }
 
 export default Whatsapp
