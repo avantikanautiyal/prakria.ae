ebce67c427b3caab1e374a934d78585b3890e21a
Nikhil Kumar <nikhilkumar@Nikhils-MacBook-Air.local>
2025-09-15 16:55:07 +0530

    some tag add

--- src/app/layout.js
+++ src/app/layout.js
@@ -41,10 +41,34 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="shortcut icon" href="/images/favicon.ico" />
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KMGMRW7H');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+
       </head>
       <body
         className={`${signika.className} ${inter.variable} ${hankenGrotesk.variable} dark`}
       >
+
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KMGMRW7H"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         {/* Google tag (gtag.js) */}
         <Script
           strategy="afterInteractive"
@@ -58,6 +82,10 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-TDS9XMGZG2');
           `}
         </Script>
+
+
+
+
         <Whatsapp />
         <SocialWidget />
         {children}
