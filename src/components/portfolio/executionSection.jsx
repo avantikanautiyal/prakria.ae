d3282adc64c897da9ca222de09a39ab67784e870
sourav <souravsh1234567@gmail.com>
2025-10-30 14:54:31 +0530

    updated

--- src/components/portfolio/executionSection.jsx
+++ src/components/portfolio/executionSection.jsx
@@ -24,13 +24,14 @@ function ExecutionSection(props) {
           <div className="hidden md:block absolute top-[3.5rem] left-0 right-0 h-1 border-t-2 border-dashed border-gray-600 w-full transform -translate-y-1/2"></div>
 
           {/* Grid for timeline items */}
-          <div className={`grid grid-cols-1 md:${"grid-cols-" +props?.list?.length} gap-y-12 md:gap-x-8 lg:gap-x-12 z-10`}>
-
+          <div
+            className="flex flex-col md:flex-row md:justify-between md:items-stretch gap-y-12 md:gap-y-0 md:gap-x-8 lg:gap-x-12 z-10 w-full"
+          >
             {props?.list?.map((sec, index) => {
               return (
                 <div
                   key={index}
-                  className="flex flex-col items-center text-center relative"
+                  className="flex flex-col items-center text-center relative flex-1"
                 >
                   <div className="w-24 h-24 sm:w-28 sm:h-28 rounded-full bg-gray-800 flex items-center justify-center mb-6 shadow-lg border-gray-700">
                     <img
@@ -48,7 +49,6 @@ function ExecutionSection(props) {
                 </div>
               );
             })}
-       
           </div>
         </div>
       </div>
