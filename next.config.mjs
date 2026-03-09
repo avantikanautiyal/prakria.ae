e3597581e749319f5c91a0d011286d39dd316d87
Nikhil Kumar <nikhilkumar@Nikhils-MacBook-Air.local>
2026-03-09 14:50:26 +0530

    output standalone

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'standalone',
     experimental: {
         serverActions: {
             bodySizeLimit: '100mb',
         },
     },
+
 };
 
 export default nextConfig;
